8c5e9ab2a19db777c47f4cf86466b1dbf2f0eea0
tanishenigma <tanish23enigma@gmail.com>
2025-10-05 00:53:23 +0530

    connected Frontend with backend

--- part3-notes-backend/index.js
+++ part3-notes-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 let persons = require("./data.js");
 require("dotenv").config();
 
@@ -14,12 +15,14 @@ const generateId = () => {
     persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
   return String(maxId + 1);
 };
+morgan.token("body", (req) => JSON.stringify(req.body));
 
 app.use(express.json());
-morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+
 app.get("/", (_, res) => {
   res.send(persons);
 });
@@ -44,6 +47,20 @@ app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
   persons = persons.filter((person) => person.id !== id);
   res.status(204).end();
+});
+
+app.put("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const person = persons.find((p) => p.id === id);
+  if (!person) {
+    return res.status(404).json({ error: "person not found" });
+  }
+
+  const updatedPerson = { ...person, ...body }; // merges new fields
+  persons = persons.map((p) => (p.id === id ? updatedPerson : p));
+
+  res.json(updatedPerson);
 });
 
 app.post("/api/persons", (req, res) => {
